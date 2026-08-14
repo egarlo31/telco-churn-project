79e0fdf7bc84126f0f51df425207c8e5217d933d
emi.garlo <egarlo31@icloud.com>
2026-08-13 18:57:07 -0600

    feat: add basic FastAPI endpoints and request schema

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,3 +11,17 @@ def get_number():
 def create_number(cantidad: int):
     return {"cantidad": cantidad,
             "double": cantidad*2}
+
+@app.get("/double")
+def get_double(cantidad: int):
+    return {"resultado": cantidad + cantidad}
+
+class Operation(BaseModel):
+    number_one: float
+    number_two: float
+
+@app.post("/sum")
+def sum_numbers(data: Operation):
+    result = (data.number_one / data.number_two)**2
+
+    return {"result": result}
